88086f462c0b2431d1a2e8acea780cf202fed44f
Sattyaj Paul <sattyaj.paul@hotmail.com>
2025-02-11 13:47:32 +0000

    Update twoSum.py

--- Arrays and Hashing/twoSum.py
+++ Arrays and Hashing/twoSum.py
@@ -10,5 +10,5 @@ class Solution:
             if diff in prevMap: # look for the number you need in the 
                 return [prevMap[diff], i] # if the number is already in the map then rreturn the corresponding value (which is an index) adn the original index of current number in the list
             prevMap[n] = i # if the number isnt found add it to the map with n as key and i as the value 
-        return
+        return sasd
     
