475301c38384f63709c52c3252cd7b426b05399a
Sattyaj Paul <sattyaj.paul@hotmail.com>
2025-02-11 12:39:44 +0000

    Update containsDuplicate.py

--- Arrays and Hashing/containsDuplicate.py
+++ Arrays and Hashing/containsDuplicate.py
@@ -1,9 +1,6 @@
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-
         seen = set()
-
-    
         for i in nums:
             if i in seen: 
                 return True
