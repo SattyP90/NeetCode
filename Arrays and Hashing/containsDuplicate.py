d8adbeaa427dfd6bcbc27cd08355c0108dc8e249
Sattyaj Paul <sattyaj.paul@hotmail.com>
2025-02-11 12:41:40 +0000

    Update containsDuplicate.py

--- Arrays and Hashing/containsDuplicate.py
+++ Arrays and Hashing/containsDuplicate.py
@@ -1,12 +1,14 @@
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
+
+        #create a set to store the values that have been seen
         seen = set()
-        for i in nums:
-            if i in seen: 
-                return True
+        for i in nums: #iterate through the list 
+            if i in seen: #check if i has been seeen
+                return True # if so then return true and exit the loop
             else:
-                seen.add(i)
+                seen.add(i)#or else add the new item into see hashset
         
-        return False
+        return False #if nothing is found return false
 
 
